5c530c1a63c94a534642002d021ddf56c535a8aa
chiara-moretti <chiara1.moretti@mail.polimi.it>
2026-03-25 17:40:05 +0100

    create ClickCounter component

---
+++ src/lib/components/ClickCounter.svelte
@@ -0,0 +1,17 @@
+<script>
+    console.log("Hello world")
+
+    let count = $state(0);
+    // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
+
+    let doubled = $derived(count*2);
+    // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
+
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button 
+    on:click={() => count++}>
+    click: {count}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,23 @@
 <script>
     console.log("Hello world")
 
-    let count = $state(2);
+    let count = $state(0);
     // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
 
-    let doubled = $derived count = 2;
+    let doubled = $derived(count*2);
     // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
+
+    import ClickCounter from "$lib/components/ClickCounter.svelte";
+    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
+    // è possibile importare componenti da altri file e usarli come tag HTML
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button 
-    on:click={() => count++}>
-    click: {count}
-</button>
-
-<span>double is {doubled}</span>
+<ClickCounter />
+<ProgressiveSum />
 
 <style>
     h1 {
