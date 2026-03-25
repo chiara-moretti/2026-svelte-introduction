8143328162e84b01832c7ace0556896242ec7228
chiara-moretti <chiara1.moretti@mail.polimi.it>
2026-03-25 17:28:47 +0100

    add state and derived property

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script>
     console.log("Hello world")
+
+    let count = $state(2);
+    // lo stato è reattivo, quindi quando viene modificato, il componente si aggiorna automaticamente
+
+    let doubled = $derived count = 2;
+    // anche le variabili che dipendono dallo stato sono reattive, quindi quando count cambia, anche doubled cambia automaticamente
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<button 
+    on:click={() => count++}>
+    click: {count}
+</button>
+
+<span>double is {doubled}</span>
+
 <style>
     h1 {
         color: deepskyblue;
     }
-
-    
 </style>
+
